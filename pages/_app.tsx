b3269e58ef0e0ab73f45ed17c8bfb347e8a95ed3
eastcopper <ldh7228@gmail.com>
2022-03-23 15:51:29 +0900

    ⚡ :: (#10)[App] Add Top and Footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import "semantic-ui-css/semantic.min.css";
+import Footer from "../src/components/Footer";
+import Top from "../src/components/Top";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Top />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/view/[id].tsx
+++ pages/view/[id].tsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <div>
-      <div>asdf</div>
+      <div>ID</div>
     </div>
   );
 }
