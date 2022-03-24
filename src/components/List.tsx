421f53521cba5504b6ac6563d209b8e0851b60fa
eastcopper <ldh7228@gmail.com>
2022-03-25 08:50:43 +0900

    ⚡ :: (#16)[ItemList] Add Link

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Grid, Image, Button, Icon, Label } from "semantic-ui-react";
 
 export default function ItemList({ item }: any) {
@@ -8,23 +9,27 @@ export default function ItemList({ item }: any) {
         <Grid.Row>
           {item.map((list: any, index: number) => (
             <Grid.Column key={index}>
-              <Image src={list.image_link} />
-              <div>{list.name}</div>
-              <div>
-                <div>
-                  {"$"}
-                  {list.price}
-                </div>
-                <Button as="div" labelPosition="right">
-                  <Button color="yellow" style={{ width: "100px" }}>
-                    <Icon name="star" />
-                    Like
-                  </Button>
-                  <Label as="a" basic color="yellow" pointing="left">
-                    {list.rating === null ? 0 : list.rating}
-                  </Label>
+              <Link href={`/view/${list.id}`}>
+                <a>
+                  <Image src={list.image_link} />
+                  <div>{list.name}</div>
+                  <div>
+                    <div>
+                      {"$"}
+                      {list.price}
+                    </div>
+                  </div>
+                </a>
+              </Link>
+              <Button as="div" labelPosition="right">
+                <Button color="yellow" style={{ width: "100px" }}>
+                  <Icon name="star" />
+                  Like
                 </Button>
-              </div>
+                <Label as="a" basic color="yellow" pointing="left">
+                  {list.rating === null ? 0 : list.rating}
+                </Label>
+              </Button>
             </Grid.Column>
           ))}
         </Grid.Row>
