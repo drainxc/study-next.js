b15efcf4c99acc034b0912db1e2f9889ccfb87a5
eastcopper <ldh7228@gmail.com>
2022-03-25 08:36:41 +0900

    ⚡ :: (#15)[ItemList] Design ItemList

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,17 +1,34 @@
-import { Image, List } from "semantic-ui-react";
+import { Grid, Image, Button, Icon, Label } from "semantic-ui-react";
 
 export default function ItemList({ item }: any) {
   console.log(item);
   return (
-    <List divided verticalAlign="middle">
-      {item.map((list: any, index: number) => (
-        <List.Item key={index}>
-          <Image avatar src={list.image_link} />
-          <List.Content>
-            <List.Header as="a">{list.name}</List.Header>
-          </List.Content>
-        </List.Item>
-      ))}
-    </List>
+    <>
+      <Grid columns={5}>
+        <Grid.Row>
+          {item.map((list: any, index: number) => (
+            <Grid.Column key={index}>
+              <Image src={list.image_link} />
+              <div>{list.name}</div>
+              <div>
+                <div>
+                  {"$"}
+                  {list.price}
+                </div>
+                <Button as="div" labelPosition="right">
+                  <Button color="yellow" style={{ width: "100px" }}>
+                    <Icon name="star" />
+                    Like
+                  </Button>
+                  <Label as="a" basic color="yellow" pointing="left">
+                    {list.rating === null ? 0 : list.rating}
+                  </Label>
+                </Button>
+              </div>
+            </Grid.Column>
+          ))}
+        </Grid.Row>
+      </Grid>
+    </>
   );
 }
