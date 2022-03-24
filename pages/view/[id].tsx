7805ca7c45b142d4e5cecdb414c1eff668a48048
eastcopper <ldh7228@gmail.com>
2022-03-25 08:51:03 +0900

    ⚡ :: (#17)[View] Add Router

--- pages/view/[id].tsx
+++ pages/view/[id].tsx
@@ -1,7 +1,12 @@
+import { useRouter } from "next/router";
+
 export default function About() {
+  const router = useRouter();
+  const { id } = router.query;
+
   return (
     <div>
-      <div>ID</div>
+      <div>{id}</div>
     </div>
   );
 }
