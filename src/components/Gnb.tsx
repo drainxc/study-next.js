8ceed814305dae5c36bf2092beef01e7da34a711
eastcopper <ldh7228@gmail.com>
2022-03-24 17:44:50 +0900

    ⚡ :: (#12)[Gnb] Update Menu

--- src/components/Gnb.tsx
+++ src/components/Gnb.tsx
@@ -7,17 +7,14 @@ export default function Gnb() {
       <Menu inverted>
         <Menu.Item
           name="home"
-          active={activeItem === "home"}
           //   onClick={this.handleItemClick}
         />
         <Menu.Item
           name="messages"
-          active={activeItem === "messages"}
           //   onClick={this.handleItemClick}
         />
         <Menu.Item
           name="friends"
-          active={activeItem === "friends"}
           //   onClick={this.handleItemClick}
         />
       </Menu>
