d786421a97d34d2ea61553b6a45c508b08b6c93a
eastcopper <ldh7228@gmail.com>
2022-03-25 11:06:55 +0900

    ⚡ :: (#24)[About] Add api

--- pages/view/[id].tsx
+++ pages/view/[id].tsx
@@ -1,13 +1,47 @@
 import axios from "axios";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import { Dimmer, Loader, Item } from "semantic-ui-react";
+import { BASE_URL } from "../../src/lib/export/data";
 
 export default function About() {
   const router = useRouter();
   const { id } = router.query;
+  const [loading, setLoading] = useState<boolean>(true);
+  const [items, SetItems] = useState([
+    {
+      childKey: 0,
+      image: "",
+      header: "",
+      description: "",
+      meta: "",
+      extra: "",
+    },
+  ]);
+
+  axios
+    .get(BASE_URL + `api/v1/products/${id}.json`)
+    .then((res) => {
+      console.log(res);
+      setLoading(false);
+
+      SetItems([
+        {
+          childKey: 0,
+          image: res.data.image_link,
+          header: res.data.name,
+          description: res.data.description,
+          meta: res.data.brand,
+          extra: "$" + res.data.price,
+        },
+      ]);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
   return (
-    <div>
-      <div>{id}</div>
-    </div>
+    <>
+    </>
   );
 }
