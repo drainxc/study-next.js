5eab983486dbb540975195078070cdb3ba02337e
eastcopper <ldh7228@gmail.com>
2022-03-28 14:06:53 +0900

    ⚡ :: (#28)[Data] Add CLIENT_ID

--- src/lib/export/data.js
+++ src/lib/export/data.js
@@ -1,1 +1,5 @@
 export const BASE_URL = "http://makeup-api.herokuapp.com/";
+
+export const CLIENT_ID =
+  "822843989363-k3pgoidc2g7v0mcba4ijjd0g3rpg21c2.apps.googleusercontent.com";
+export const CLIENT_PASSWORD = "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";
