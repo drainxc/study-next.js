4cce6f47b76d4d81f35f47a4c51efaf5f5090e6d
eastcopper <ldh7228@gmail.com>
2022-04-05 20:24:51 +0900

    ⚡ :: (#34)[Gnb] Update Gnb

--- src/components/gnb/index.tsx
+++ src/components/gnb/index.tsx
@@ -1,21 +1,32 @@
+import { useRouter } from "next/router";
 import { Menu } from "semantic-ui-react";
 
 export default function Gnb() {
+  const router = useRouter();
+  console.log(router);
+  let active = "";
+  if (router.pathname === "/") {
+    active = "home";
+  } else if (router.pathname === "/view/[id]") {
+    active = "view";
+  } else if (router.pathname === "/view/login") {
+    active = "login";
+  }
+
+  function goLink(e: any, data: any) {
+    const { name }: any = data;
+    console.log(typeof data);
+    if (name === "home") {
+      router.push("/");
+    }
+  }
+
   return (
     <>
       <Menu inverted>
-        <Menu.Item
-          name="home"
-          //   onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name="messages"
-          //   onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name="friends"
-          //   onClick={this.handleItemClick}
-        />
+        <Menu.Item name="home" active={active === "home"} onClick={goLink} />
+        <Menu.Item name="view" active={active === "view"} onClick={goLink} />
+        <Menu.Item name="login" active={active === "login"} onClick={goLink} />
       </Menu>
     </>
   );
